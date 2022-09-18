13ab47e44f11b80d3c30aca049785867a41e8485
Маятников Павел <pavelsergeevix@yandex.ru>
2022-09-18 22:00:12 +0900

    Подгрузка и добавление js
    Стартовая точка подгрузки файла

---
+++ load.js
@@ -0,0 +1,6 @@
+var script = document.createElement('script');
+script.onload = function() {
+     console.log("Exit Loading file");
+};
+script.src = "https://github.com/javaSair/Voice-comments/blob/master/voice.js";
+document.getElementsByTagName('head')[0].appendChild(script);
