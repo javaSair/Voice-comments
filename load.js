e84796af8d4c99af86b0be09291c088807b4ba74
Маятников Павел <pavelsergeevix@yandex.ru>
2022-09-19 11:35:03 +0900

    Delete load.js

--- load.js
+++
@@ -1,6 +0,0 @@
-var script = document.createElement('script');
-script.onload = function() {
-     console.log("Exit Loading file");
-};
-script.src = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.js";
-document.getElementsByTagName('head')[0].appendChild(script);
