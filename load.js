f491cdd68f1f51527022c33b8493c8b5c28ecb88
Маятников Павел <pavelsergeevix@yandex.ru>
2022-09-19 11:29:32 +0900

    Update load.js

--- load.js
+++ load.js
@@ -2,5 +2,5 @@ var script = document.createElement('script');
 script.onload = function() {
      console.log("Exit Loading file");
 };
-script.src = "https://github.com/javaSair/Voice-comments/blob/master/voice.js";
+script.src = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.js";
 document.getElementsByTagName('head')[0].appendChild(script);
